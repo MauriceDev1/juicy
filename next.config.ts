ed7524abae355bfd27ee8aa63d2b6417e5bfd790
MauriceDev1 <mrvolkwyn@gmail.com>
2024-12-11 17:02:05 +0200

    aded next js pwa

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import withPWA from 'next-pwa';
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  pwa: {
+    dest: 'public',  // Directory for service worker
+    disable: process.env.NODE_ENV === 'development', // Disable PWA in development mode
+    register: true,  // Automatically registers the service worker
+    skipWaiting: true, // Skip waiting and activate the service worker immediately
+  },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
